bd37ac01a211ea0026f251187367229a12ee5b7d
shailendra pratap <singhlalprat@gmail.com>
2026-02-25 23:37:40 +0530

    Remove node_modules folder

---
+++ src/app.js
@@ -0,0 +1,18 @@
+   const express = require("express");
+    const app = express();
+
+    app.use("/",(req,res)=>{
+        res.send("Hello from the dashboard")
+    });
+   
+    app.use(". /hello",(req,res)=>{
+        res.send("Hello hello hello")
+    });
+
+    app.use("./test",(req,res)=>{
+        res.send("llo from the dashboardr")
+    })
+
+    app.listen(7777,()=>{
+        console.log("Seer is successfully listening on port 7777.....")
+    })
