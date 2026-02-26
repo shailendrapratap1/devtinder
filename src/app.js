68c2039d507fac882f143b36c9dc8f9a8e51d584
shailendra pratap <singhlalprat@gmail.com>
2026-02-26 13:27:05 +0530

    postman api http methods calling

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
    const express = require("express");
     const app = express();
 
-   
-   
+app.get("/user",(req,res)=>{
+    res.send({firstName:"akhshat",
+        lastname:"singh"
+    })
+})
+app.post("/user",(req,res)=>{
+    res.send("user is resent")
+})
+app.put("/user",(req,res)=>{
+    res.send("putted the user")
+})
+app.patch("/hello",(req,res)=>{
+    res.send("user has been patched")
+})
+app.delete("/user",(rq,res)=>{
+    res.send("user has been deleted")
+})
     app.use("/hello",(req,res)=>{
         res.send("Hello hello hello")
     });
+    app.head("/user",(req,res)=>{
+        res.send("user result has been headed")
+    })
+    app.options("/user",(req,res)=>{
+        res.send("user has options")
+    })
 
     app.use("/test",(req,res)=>{
         res.send("llo from the dashboardr")
