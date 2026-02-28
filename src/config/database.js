428d1bd9920e77efb01a47fe0e73f27a1c0fe049
shailendra pratap <singhlalprat@gmail.com>
2026-02-28 17:17:06 +0530

    connecting dbstring

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+  const mongoose = require('mongoose');
+
+const connectDB = async() =>{
+    await mongoose.connect(
+        "mongodb+srv://namaste:AcsTC2sf5Ny1qe6Q@namastenode.ndnjmam.mongodb.net/devTinder"
+    )
+}
+
+module.exports = connectDB; 
+
+// connectDB().then(()=>{
+//  console.log("Database connection established")
+// }).catch(err=>{
+// console.log("Database cannot be connected")
+// })
