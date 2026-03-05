5e302bcd63c62c8da6fe44194e541f73dc083c50
shailendra pratap <singhlalprat@gmail.com>
2026-03-05 18:15:36 +0530

    email and password validators

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName:{
@@ -17,10 +18,20 @@ const userSchema = new mongoose.Schema({
         required:true,
         unique:true,
       trim:true,
+      validate(value){
+        if(!validator.isEmail(value)){
+            throw new Error("Invalid email address: " + value);
+        }
+      }
     },
     password: {
         type:String,
-        required:true
+        required:true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("password is too weak: " + value)
+            }
+        }
     },
    age: {
 type:   Number,
@@ -38,7 +49,11 @@ min:18
     photoUrl:{
         type:String,
         default:"https://www.bing.com/images/search?view=detailV2&ccid=i%2f1HL4I1&id=36293A16CA5963514E1D4AC288D98B55CC1135F0&thid=OIP.i_1HL4I1H9SznR11omKPFwHaHo&mediaurl=https%3a%2f%2fcanto-wp-media.s3.amazonaws.com%2fapp%2fuploads%2f2019%2f08%2f19194138%2fimage-url-3.jpg&exph=824&expw=800&q=image+url+free&FORM=IRPRST&ck=D83B0DEB3D6E8839583550834CB38EAD&selectedIndex=12&itb=0"
-    },
+    ,     validate(value){
+        if(!validator.isURL){
+            throw new Error("url is not written correctly or it is too long")
+        }
+    }},
     about:{
         type:String,
         default:"this is default about user"
@@ -51,4 +66,4 @@ min:18
 
 
 },{timestamps:true})
-module.exports = mongoose.model("User",userSchema)
+module.exports = mongoose.model("User",userSchema) 
