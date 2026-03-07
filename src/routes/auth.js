3c2f473dad56b76e74248f628727d140c69c4a09
shailendra pratap <singhlalprat@gmail.com>
2026-03-07 18:38:28 +0530

    logout api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -52,4 +52,11 @@ if(isPasswordValid){
 })
 
 
+authRouter.post("/logout",async(req,res)=>{
+    res.cookie("token",null,{
+        expires:new Date(Date.now()),
+    });
+    res.send("logged out successfully")
+})
 module.exports = authRouter
+
