2e2a99f87d0b27f46d94c5a9926727599353c15e
shailendra pratap <singhlalprat@gmail.com>
2026-03-07 10:48:32 +0530

    schema for validation and encryptionn of password

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 const validator = require("validator");
-
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 const userSchema = new mongoose.Schema({
     firstName:{
         type:String,
@@ -66,4 +67,25 @@ min:18
 
 
 },{timestamps:true})
+
+userSchema.methods.getJWT = async function(){
+    const user = this;
+
+    const token = await jwt.sign({_id: user._id},"DEV@TNDER$8578",{
+        expiresIn:"7d",
+    })
+    return token
+}
+
+userSchema.methods.validatePassword = async function (passwordInputByUser){
+    const user = this;
+    const passwordHash = user.password;
+
+    const isPasswordValid = await bcrypt.compare(
+        passwordInputByUser,
+        passwordHash
+    )
+    return isPasswordValid
+}
+
 module.exports = mongoose.model("User",userSchema) 
