f05dd01f12d2bb0487132d10d42d19efed95f3c6
shailendra pratap <singhlalprat@gmail.com>
2026-03-07 16:59:37 +0530

    authrouter request router profile router

--- src/app.js
+++ src/app.js
@@ -70,14 +70,11 @@
 // //     res.send("2nd route hanser")
 // //     // console.log(" handling the route user")
 // //      next()
-    
-    
 // // },
 // // (req,res,next)=>{
 // //     // console.log("sending the res")
 // //     res.send(" 2nd response")
 // //     next()
-   
 // // } ,
 // // //     ],
 // // (req,res,next)=>{
@@ -147,154 +144,158 @@ const connectDB =  require("./config/database")
  const cookieParser = require("cookie-parser")
  const bcrypt = require("bcrypt")
  const jwt = require("jsonwebtoken")
- const {userAuth} = require('./middlewares/auth');
-const user = require("./models/user");
+//  const {userAuth} = require('./middlewares/auth');
+// const user = require("./models/user");
 app.use(express.json())
 app.use(cookieParser())
- app.post("/signup",async(req,res)=>{
-  // validation of data
 
-   try{
-    validateSignUpData(req)
-const{firstName,lastName, emailId, password}= req.body;
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile")
+const requestRouter = require("./routes/request")
+app.use("/",authRouter)
+app.use("/",profileRouter);
+app.use("/",requestRouter)
+//  app.post("/signup",async(req,res)=>{
+//   // validation of data
 
-const passwordHash =await bcrypt.hash(password,10);
- console.log(passwordHash)
-     const user = new User({
-      firstName,
-      lastName,
-      emailId,
-      password:passwordHash,
-     })
+//    try{
+//     validateSignUpData(req)
+// const{firstName,lastName, emailId, password}= req.body;
+
+// const passwordHash =await bcrypt.hash(password,10);
+//  console.log(passwordHash)
+//      const user = new User({
+//       firstName,
+//       lastName,
+//       emailId,
+//       password:passwordHash,
+//      })
    
-     await user.save()
-     res.send("user added successfully")
-   }catch(err){
-     res.status(400).send("Error saving the user:"+ err.message)
-   }
+//      await user.save()
+//      res.send("user added successfully")
+//    }catch(err){
+//      res.status(400).send("Error saving the user:"+ err.message)
+//    }
   
   
- })
+//  })
 
- app.get("/user",async(req,res)=>{
-  const  userEmail = req.body.emailId;
+//  app.get("/user",async(req,res)=>{
+//   const  userEmail = req.body.emailId;
   
-  try{
-    const users = await User.find({emailId:userEmail});
-    if(users.length === 0){
-      res.status(404).send("User not found");
-    }else{
-      res.send(users);
-    }
-  }catch(err){
-    res.status(400).send("Something went wrong")
-  }
- })
-app.get("/feed",async(req,res)=>{
+//   try{
+//     const users = await User.find({emailId:userEmail});
+//     if(users.length === 0){
+//       res.status(404).send("User not found");
+//     }else{
+//       res.send(users);
+//     }
+//   }catch(err){
+//     res.status(400).send("Something went wrong")
+//   }
+//  })
+// app.get("/feed",async(req,res)=>{
 
-  try{
-    const users = await User.find({});
-    res.send(users)
-  }
-  catch(err){
-    res.status(400).send("Something went wrong")
-  }
-})
+//   try{
+//     const users = await User.find({});
+//     res.send(users)
+//   }
+//   catch(err){
+//     res.status(400).send("Something went wrong")
+//   }
+// })
 
-app.get("/user",async(req,res)=>{
-  const userEmail = req.body.emailId;
+// app.get("/user",async(req,res)=>{
+//   const userEmail = req.body.emailId;
 
-  try{
-  const user = await User.findOne({emailId:userEmail});
-  res.send(user)
-  }catch(err){
-    res.status(400).send("Something went wrong")
-  }
-})
+//   try{
+//   const user = await User.findOne({emailId:userEmail});
+//   res.send(user)
+//   }catch(err){
+//     res.status(400).send("Something went wrong")
+//   }
+// })
 
-app.delete("/user",async(req,res) => {
-  const userId = req.body.userId;
-  try{
-    const user = await User.findByIdAndDelete(userId);
-    res.send("User deleted successfully")
-  }catch(err){
-    res.status(400).send("something went wrong")
-  }
-})
+// app.delete("/user",async(req,res) => {
+//   const userId = req.body.userId;
+//   try{
+//     const user = await User.findByIdAndDelete(userId);
+//     res.send("User deleted successfully")
+//   }catch(err){
+//     res.status(400).send("something went wrong")
+//   }
+// })
 
-// login api
+// // login api
 
-app.post("/login",async(req,res)=>{
-  try{
-    const {emailId,password}= req.body;
+// app.post("/login",async(req,res)=>{
+//   try{
+//     const {emailId,password}= req.body;
 
-    const user = await User.findOne({emailId:emailId});
-    if(!user){
-      throw new Error("Invalid credentials")
-    }
-    const isPasswordValid = await user.validatePassword(password)
-    if(isPasswordValid){
+//     const user = await User.findOne({emailId:emailId});
+//     if(!user){
+//       throw new Error("Invalid credentials")
+//     }
+//     const isPasswordValid = await user.validatePassword(password)
+//     if(isPasswordValid){
 
-    // Create a jwt token
+//     // Create a jwt token
 
-    // create jwt token 
+//     // create jwt token 
 
-    // ADD THE TOKEN
-const token = await user.getJWT( )
+//     // ADD THE TOKEN
+// const token = await user.getJWT( )
+//     res.cookie("token",token,{
+//           expires: new Date(Date.now()+ 8 * 3600000),
+//     })
 
-    res.cookie("token",token,{
-          expires: new Date(Date.now()+ 8 * 3600000),
-    })
+//       res.send("Login successfull!!!")
+//     }else throw new Error("Invalid credentials")
+//   }catch(err){
+//     res.status(400).send("ERROR : " + err.message)
+//   }
+// })
+// app.get("/profile",userAuth, async(req,res)=>{
+//    try{
+//   const user = req.user
+//   res.send(user);
+//    }
+//   catch(err){
+//     res.status(400).send("ERROR : " + err.message)
+//   }
+//   })
 
-      res.send("Login successfull!!!")
-    }else throw new Error("Invalid credentials")
-  }catch(err){
-    res.status(400).send("ERROR : " + err.message)
-  }
-})
-app.get("/profile",userAuth, async(req,res)=>{
-   try{
-  
+//   app.post("/sendConnectionRequest",userAuth,async(req,res)=>{
+//     const user = req.user;
 
-  const user = req.user
-  res.send(user);
-   }
-  catch(err){
-    res.status(400).send("ERROR : " + err.message)
-  }
-  })
+//     console.log("sending a connecton request ")
+//     res.send(user.firstName + "sent the connection request at" )
+//   })
 
-  app.post("/sendConnectionRequest",userAuth,async(req,res)=>{
-    const user = req.user;
-
-    console.log("sending a connecton request ")
-    res.send(user.firstName + "sent the connection request at" )
-  })
-
-app.patch("/user/:userId",async(req,res)=>{
-  const userId= req.params?.userId;
-  const data = req.body;
+// app.patch("/user/:userId",async(req,res)=>{
+//   const userId= req.params?.userId;
+//   const data = req.body;
  
-  try{
-const ALLOWED_UPDATES = ["photoUrl","about","gender","age","skills"];
-const isUpdateAllowed = Object.keys(data).every((k)=>
-ALLOWED_UPDATES.includes(k)
-);
-if(!isUpdateAllowed){
-  throw new Error("Update not allowed")
-}
-if(data?.skills.length>10){
-  throw new Error("length should not exceed the limit")
-}
+//   try{
+// const ALLOWED_UPDATES = ["photoUrl","about","gender","age","skills"];
+// const isUpdateAllowed = Object.keys(data).every((k)=>
+// ALLOWED_UPDATES.includes(k)
+// );
+// if(!isUpdateAllowed){
+//   throw new Error("Update not allowed")
+// }
+// if(data?.skills.length>10){
+//   throw new Error("length should not exceed the limit")
+// }
 
-    const user = await User.findByIdAndUpdate({_id:userId},data,{returnDocument:"after",runValidators:true})
-   console.log(user)
-    res.status(200).send("user update successfully")
+//     const user = await User.findByIdAndUpdate({_id:userId},data,{returnDocument:"after",runValidators:true})
+//    console.log(user)
+//     res.status(200).send("user update successfully")
   
-  }catch(err){
-    res.status(400).send("something went wrong" + err.message)
-  }
-})
+//   }catch(err){
+//     res.status(400).send("something went wrong" + err.message)
+//   }
+// })
 
  connectDB().then(()=>{
  console.log("Database connection established")
